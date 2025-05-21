4d07352cb75c6750144eb2b45e26db9580ff119f
Vitaly L <111348000+vetrof@users.noreply.github.com>
2025-05-21 14:53:34 +0500

    add django
    django for admin

---
+++ admin/django/places/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from leaflet.admin import LeafletGeoAdmin
+
+from places.models import Place
+
+
+@admin.register(Place)
+class PlaceAdmin(LeafletGeoAdmin):
+    list_display = ("city_name", "name", "geom")
